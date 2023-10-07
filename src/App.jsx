76c7daf7240744cfde81fd2f98263ab594b08afa
Emre Ilhan <emrilhan@gmail.com>
2023-10-07 18:24:39 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,9 @@ import { useState } from "react";
 import AddItem from "./AddItem";
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One half pound bag of Cocoa Covered Almonds Unsalted",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3",
-    },
-  ]);
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shopping list"))
+  );
 
   const [newItem, setNewItem] = useState("");
 
