2f6131844d41e2df1d07529cb15946d82430f3b2
Emre Ilhan <emrilhan@gmail.com>
2023-10-08 17:56:40 +0300

    office computer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import Header from "./Header";
+import SearchItem from "./SearchItem";
+import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
 import { useState } from "react";
-import AddItem from "./AddItem";
 
 function App() {
   const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("shopping list"))
+    JSON.parse(localStorage.getItem("shoppinglist"))
   );
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
 
   const setAndSaveItems = (newItems) => {
     setItems(newItems);
-    localStorage.setItem("shopping list", JSON.stringify(newItems));
+    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
   };
 
   const addItem = (item) => {
@@ -50,8 +51,11 @@ function App() {
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
+      <SearchItem search={search} setSearch={setSearch} />
       <Content
-        items={items}
+        items={items.filter((item) =>
+          item.item.toLowerCase().includes(search.toLowerCase())
+        )}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
