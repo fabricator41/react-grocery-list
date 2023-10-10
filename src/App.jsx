c3a26928c7f20ec0c4e9b9eb3eebb0ae8dfd49e9
Emre Ilhan <emrilhan@gmail.com>
2023-10-10 16:45:05 +0300

    useEffect hook applied

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import SearchItem from "./SearchItem";
 import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("shoppinglist"))
+    JSON.parse(localStorage.getItem("shoppinglist")) || []
   );
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
+
+  useEffect(() => {}, []);
 
   const setAndSaveItems = (newItems) => {
     setItems(newItems);
